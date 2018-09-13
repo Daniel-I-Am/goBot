64389b0a2a6eae6bfe092369ec3e33258423b374
Daniel-I-Am <djdecloet@gmail.com>
2018-09-13 19:08:44 +0200

    start bot

--- main.go
+++ main.go
@@ -2,8 +2,62 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	
+	"github.com/bwmarrin/discordgo"
 )
 
+const token string = "NDQ0MzMzNzgyMjk2ODIxNzkw.DilAYQ.592mvqUviAGXD22X3yKusgojTog"
+var BotID string
+
 func main() {
-	fmt.Println("Hello")
+	dg, err := discordgo.New("Bot " + token)
+	checkError(err)
+	user, err := dg.User("@me")
+	checkError(err)
+	BotID = user.ID
+	fmt.Println("Bot is running as'", user.Username, "'")
+
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
