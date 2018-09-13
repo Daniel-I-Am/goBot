4dab0ba7d2cca1d343cafb28e1ba1c1bd91428fc
Daniel-I-Am <djdecloet@gmail.com>
2018-09-13 21:27:58 +0200

    roll functionality working with exception handlin

--- main.go
+++ main.go
@@ -11,21 +11,23 @@ import (
 	"strconv"
 	"regexp"
 	"math/rand"
+	"time"
 
+	"github.com/Knetic/govaluate"
 	"github.com/bwmarrin/discordgo"
 )
 
 const prefix string = "!"
-var token string
+var Token string
 var BotID string
 
 func init() {
-	flag.StringVar(&token, "t", "", "Bot token")
+	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
 func main() {
-	dg, err := discordgo.New("Bot " + token)
+	dg, err := discordgo.New("Bot " + Token)
 	checkError(err)
 	user, err := dg.User("@me")
 	checkError(err)
@@ -40,6 +42,8 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+	// Set up some last things
+	rand.Seed(time.Now().UnixNano())
 
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
@@ -64,9 +68,17 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
+
+	defer sendError(s, m.ChannelID)
 	// If the message is "ping" reply with "Pong!"
 	if strings.HasPrefix(m.Content, prefix + "roll") {
 		s.ChannelMessageSend(m.ChannelID, roll(m.Content))
+	}
+}
+
+func sendError(ses *discordgo.Session, cid string) {
+	if err := recover(); err != nil {
+		ses.ChannelMessageSend(cid, "Sorry, something went wrong.\nhttps://media.makeameme.org/created/you-caused-a-5b9ab5.jpg")
 	}
 }
 
@@ -87,21 +99,24 @@ func roll(content string) string {
 		input = match[1] + parseRoll(match[2]) + match[3]
 	}
 	// now that we have the dice rolled, we need to calculate the result
-	return input
+	expression, _ := govaluate.NewEvaluableExpression(input);
+	result, _ := expression.Evaluate(nil);
+	fmt.Println(input, "->", result)
+	return input + " = " + strconv.FormatFloat(result.(float64), 'f', -1, 64)
 }
 
 func parseRoll(input string) string {
 	regex := regexp.MustCompile("(?P<a>\\d+)d(?P<b>\\d+)")
 	if !regex.MatchString(input) { return "" }
 	match := regex.FindStringSubmatch(input)
-	toRet := "( "
+	toRet := "("
 	diceCount, _ := strconv.Atoi(match[1])
 	diceSize, _ := strconv.Atoi(match[2])
 	for i := 0; i < diceCount; i++ {
 		rn := rand.Intn(diceSize)
-		toRet += strconv.Itoa(rn) + " + "
+		toRet += strconv.Itoa(rn) + "+"
 	}
-	toRet = toRet[:len(toRet)-3]
-	toRet += " )"
+	toRet = toRet[:len(toRet)-1]
+	toRet += ")"
 	return toRet
 }
